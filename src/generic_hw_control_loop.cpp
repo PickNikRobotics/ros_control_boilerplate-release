9f1e9b9397934d2b963fef2a0c19add3a5299331
JafarAbdi <cafer.abdi@gmail.com>
2021-02-23 16:18:36 +0300

    Rebase from 'upstream'

--- src/generic_hw_control_loop.cpp
+++ src/generic_hw_control_loop.cpp
@@ -45,7 +45,7 @@
 namespace ros_control_boilerplate
 {
 GenericHWControlLoop::GenericHWControlLoop(
-    ros::NodeHandle& nh, boost::shared_ptr<ros_control_boilerplate::GenericHWInterface> hardware_interface)
+    ros::NodeHandle& nh, boost::shared_ptr<hardware_interface::RobotHW> hardware_interface)
   : nh_(nh), hardware_interface_(hardware_interface)
 {
   // Create the controller manager
@@ -61,23 +61,31 @@ GenericHWControlLoop::GenericHWControlLoop(
   // Get current time for use with first update
   clock_gettime(CLOCK_MONOTONIC, &last_time_);
 
-  // Start timer
-  ros::Duration desired_update_freq = ros::Duration(1 / loop_hz_);
-  non_realtime_loop_ = nh_.createTimer(desired_update_freq, &GenericHWControlLoop::update, this);
+  desired_update_period_ = ros::Duration(1 / loop_hz_);
 }
 
-void GenericHWControlLoop::update(const ros::TimerEvent& e)
+void GenericHWControlLoop::run()
+{
+  ros::Rate rate(loop_hz_);
+  while(ros::ok()) {
+    update();
+    rate.sleep();
+  }
+}
+
+void GenericHWControlLoop::update()
 {
   // Get change in time
   clock_gettime(CLOCK_MONOTONIC, &current_time_);
   elapsed_time_ =
       ros::Duration(current_time_.tv_sec - last_time_.tv_sec + (current_time_.tv_nsec - last_time_.tv_nsec) / BILLION);
   last_time_ = current_time_;
+  ros::Time now = ros::Time::now();
   // ROS_DEBUG_STREAM_THROTTLE_NAMED(1, "generic_hw_main","Sampled update loop with elapsed
   // time " << elapsed_time_.toSec());
 
   // Error check cycle time
-  const double cycle_time_error = (elapsed_time_ - desired_update_freq_).toSec();
+  const double cycle_time_error = (elapsed_time_ - desired_update_period_).toSec();
   if (cycle_time_error > cycle_time_error_threshold_)
   {
     ROS_WARN_STREAM_NAMED(name_, "Cycle time exceeded error threshold by: "
@@ -86,13 +94,13 @@ void GenericHWControlLoop::update(const ros::TimerEvent& e)
   }
 
   // Input
-  hardware_interface_->read(elapsed_time_);
+  hardware_interface_->read(now, elapsed_time_);
 
   // Control
-  controller_manager_->update(ros::Time::now(), elapsed_time_);
+  controller_manager_->update(now, elapsed_time_);
 
   // Output
-  hardware_interface_->write(elapsed_time_);
+  hardware_interface_->write(now, elapsed_time_);
 }
 
 }  // namespace
