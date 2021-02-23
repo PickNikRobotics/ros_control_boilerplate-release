564cd41d4723a20b24b7fd1388aa64f564944830
JafarAbdi <cafer.abdi@gmail.com>
2021-02-23 16:32:25 +0300

    Rebase from 'debian/noetic/ros_control_boilerplate'

--- rrbot_control/src/rrbot_hw_main.cpp
+++ rrbot_control/src/rrbot_hw_main.cpp
@@ -46,17 +46,16 @@ int main(int argc, char** argv)
 
   // NOTE: We run the ROS loop in a separate thread as external calls such
   // as service callbacks to load controllers can block the (main) control loop
-  ros::AsyncSpinner spinner(2);
+  ros::AsyncSpinner spinner(3);
   spinner.start();
 
   // Create the hardware interface specific to your robot
-  boost::shared_ptr<rrbot_control::RRBotHWInterface> rrbot_hw_interface
-    (new rrbot_control::RRBotHWInterface(nh));
+  std::shared_ptr<rrbot_control::RRBotHWInterface> rrbot_hw_interface(new rrbot_control::RRBotHWInterface(nh));
   rrbot_hw_interface->init();
 
   // Start the control loop
   ros_control_boilerplate::GenericHWControlLoop control_loop(nh, rrbot_hw_interface);
-  control_loop.run(); // Blocks until shutdown signal recieved
+  control_loop.run();  // Blocks until shutdown signal recieved
 
   return 0;
 }
